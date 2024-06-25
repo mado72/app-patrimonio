d907a17f5331375ea057130b5970a352333673cf
Marcelo Damasceno <mdamasceno@globality.com.br>
2024-06-25 14:19:15 -0300

    feat: ativo.service usando http

--- app-patrimonio/src/app/services/ativo.service.ts
+++ app-patrimonio/src/app/services/ativo.service.ts
@@ -1,36 +1,8 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
-import { Observable, delay, of } from "rxjs";
+import { Observable, map } from "rxjs";
+import { environment } from "../../environments/environment";
 import { Ativo, IAtivo } from "../models/investimento.model";
-import { Moeda } from "../models/base.model";
-
-class MockData {
-    private _items: Ativo[] = [
-        new Ativo({
-            moeda: Moeda.BRL,
-            sigla: 'ATV1',
-            nome: "Ativo 1",
-            valor: 1000,
-            tipo: "Acao",
-        }),
-        new Ativo({
-            moeda: Moeda.BRL,
-            sigla: 'ATV2',
-            nome: "Ativo 2",
-            valor: 2000,
-            tipo: "Acao",
-        }),
-    ];
-    get items() {
-        return [...this._items];
-    }
-    push(ativo: Ativo) {
-        this._items.push(ativo);
-    }
-    remove(ativo: Ativo) {
-        this._items = this._items.filter(item => item.identity!== ativo.identity);
-    }
-}
 
 export type FilterAtivos = {
     termo?: string
@@ -42,32 +14,37 @@ export type FilterAtivos = {
 export class AtivoService {
     readonly http = inject(HttpClient);
 
-    mock = new MockData();
+    private _http = inject(HttpClient);
 
     getAtivos(filter?: FilterAtivos): Observable<Ativo[]> {
-        console.log(this.mock)
-        return of(this.mock.items);
+        const params = new HttpParams();
+        if (filter?.termo) {
+            params.append("nome", filter.termo);
+        }
+        return this._http.get<IAtivo[]>(`${environment.apiUrl}/ativo`, { params })
+            .pipe(
+                map(ativos => ativos.map(ativo => new Ativo(ativo)))
+            )
     }
     addAtivo(ativo: IAtivo): Observable<Ativo> {
-        const novoAtivo = (<Ativo>ativo).identity ? ativo as Ativo : new Ativo(ativo);
-        this.mock.push(novoAtivo);
-        console.log(this.mock)
-        return of(novoAtivo).pipe(
-            delay(1_000)
-        );
+        return this._http.post<IAtivo>(`${environment.apiUrl}/ativo`, ativo)
+            .pipe(
+                map(ativo => new Ativo(ativo))
+            )
     }
     updateAtivo(ativo: Ativo): Observable<Ativo> {
-        console.log(this.mock)
-        return of(ativo).pipe(
-            delay(1_000)
-        );
+        const data = { ...ativo };
+        delete data.cotacao;
+        return this._http.put<IAtivo>(`${environment.apiUrl}/ativo`, data)
+            .pipe(
+                map(ativo => new Ativo(ativo))
+            )
     }
     removeAtivo(ativo: Ativo): Observable<Ativo> {
-        this.mock.remove(ativo);
-        console.log(this.mock)
-        return of(ativo).pipe(
-            delay(1_000)
-        );
+        return this._http.delete<IAtivo>(`${environment.apiUrl}/ativo/id/${ativo._id}`)
+            .pipe(
+                map(ativo => new Ativo(ativo))
+            )
     }
 
 }
