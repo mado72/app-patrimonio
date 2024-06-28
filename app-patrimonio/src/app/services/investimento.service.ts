d6387befb082ee2e6df35acaabab87098a82699a
Marcelo Damasceno <mdamasceno@globality.com.br>
2024-06-27 21:50:16 -0300

    feat: obterAlocacoes

--- app-patrimonio/src/app/services/investimento.service.ts
+++ app-patrimonio/src/app/services/investimento.service.ts
@@ -3,6 +3,7 @@ import { AtivoService } from './ativo.service';
 import { CarteiraService } from './carteira.service';
 import { CotacaoService } from './cotacao.service';
 import { concatMap, forkJoin, map, mergeMap, switchMap } from 'rxjs';
+import { InvestimentoData } from '../models/app.models';
 
 @Injectable({
   providedIn: 'root'
@@ -16,18 +17,30 @@ export class InvestimentoService {
   ) { }
 
   obterAlocacoes() {
-    return forkJoin({ 
-      carteiras: this.carteiraService.getCarteiras(),
-      ativos: this.ativoService.getAtivos()
-    }).pipe(
-      mergeMap(({ carteiras, ativos }) => 
-        this.cotacaoService.getCotacoes(ativos).pipe(
-          map(cotacoes=>({
-            carteiras,
-            ativos,
-            cotacoes
-          }))
+    return this.ativoService.getAtivos().pipe(
+      mergeMap(ativos=>{
+        return forkJoin({
+          carteiras: this.carteiraService.getCarteiras({}),
+          cotacoes: this.cotacaoService.getCotacoes(ativos)
+        }).pipe(
+          map(item=>{
+            const mapCotacoes = new Map(item.cotacoes.map(cotacao=>[cotacao.simbolo, cotacao]));
+            ativos.filter(ativo=>ativo.siglaYahoo).forEach(ativo=>ativo.cotacao = mapCotacoes.get(ativo.siglaYahoo as string));
+
+            const mapAtivos = new Map(ativos.map(ativo=>[ativo.identity, ativo]));
+            item.carteiras.forEach(carteira=>{
+              carteira.ativos.forEach(item=>item.ativo = mapAtivos.get(item.ativoId));
+              carteira.ativos = carteira.ativos.sort((a, b)=>a.ativo && b.ativo ? a.ativo?.sigla.localeCompare(b.ativo?.sigla) : (a.ativo ? -1 : 1))
+            })
+
+            return {ativos, carteiras: item.carteiras, cotacoes: item.cotacoes};
+          }),
+          map(investimentoData=>({
+            carteiras: investimentoData.carteiras.sort((a,b)=>a.nome.localeCompare(b.nome)),
+            ativos: investimentoData.ativos.sort((a,b)=>a.sigla.localeCompare(b.sigla)),
+            cotacoes: investimentoData.cotacoes.sort((a,b)=>a.simbolo.localeCompare(b.simbolo))
+          } as InvestimentoData))
         )
-      ))
+      }));
   }
 }
