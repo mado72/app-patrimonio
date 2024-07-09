28c7414ddafbf02caf763288184943c9db764810
Marcelo Damasceno <mdamasceno@globality.com.br>
2024-07-09 15:20:28 -0300

    feat: acrescenta pipes

---
+++ app-patrimonio/src/app/pipes/absolute.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'absolute',
+  standalone: true
+})
+export class AbsolutePipe implements PipeTransform {
+
+  transform(value: number, signal : boolean = false): number | string {
+    if (signal) {
+      if (isNaN(value)) return NaN
+      const isNegative = value < 0
+      return isNegative? '-' : '';
+    }
+    return Math.abs(value);
+  }
+
+}
---
+++ app-patrimonio/src/app/pipes/num-format.pipe.ts
@@ -0,0 +1,23 @@
+import { formatCurrency } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'numFormat',
+  standalone: true
+})
+export class NumFormatPipe implements PipeTransform {
+
+  transform(value: number, locale: string, currency: string, currencyCode?: string | undefined, digitsInfo?: string | undefined): string {
+    if (!value || isNaN(value)) return "-";
+
+    let negative = value < 0;
+
+    let valor = formatCurrency(Math.abs(value), locale || 'pt', currency || 'BRL', currencyCode, digitsInfo);
+
+    if (negative) {
+      valor = `(${valor})`;
+    }
+    return valor;
+  }
+
+}
