1ef6f890ad98400a9958587faa11fcf3ba35c731
Marcelo Damasceno <mdamasceno@globality.com.br>
2024-07-26 14:24:33 -0300

    chore: comenta e formata consolidacao.service

--- app-patrimonio/src/app/services/consolidacao.service.spec.ts
+++ app-patrimonio/src/app/services/consolidacao.service.spec.ts
@@ -1,16 +1,49 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ConsolidacaoService } from './consolidacao.service';
+import { InvestimentoStateService } from '../state/investimento-state.service';
+import { of } from 'rxjs';
+import { Moeda } from '../models/base.model';
+import { Alocacao, Alocacoes } from '../models/aportes.model';
 
 describe('ConsolidacaoService', () => {
   let service: ConsolidacaoService;
+  let investimentoStateService: InvestimentoStateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: InvestimentoStateService, useValue: { calcularTotaisTodasCarteiras: jest.fn(), converteParaMoeda: jest.fn() } }
+      ]
+    });
     service = TestBed.inject(ConsolidacaoService);
+    investimentoStateService = TestBed.inject(InvestimentoStateService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should calculate percentual of financeiro correctly', (done) => {
+    const mockConsolidados = [
+      { identity: 1, classe: 'A', moeda: Moeda.USD, nome: 'Carteira 1', vlTotal: 100, objetivo: 0.50 },
+      { identity: 2, classe: 'B', moeda: Moeda.BRL, nome: 'Carteira 2', vlTotal: 200, objetivo: 1.00 }
+    ];
+
+    const mockAlocacoes: Alocacao[] = [
+      { idCarteira: '1', classe: 'A (USD)', carteira: 'Carteira 1', financeiro: 100, planejado: 0.50 },
+      { idCarteira: '2', classe: 'B (EUR)', carteira: 'Carteira 2', financeiro: 200, planejado: 1.00 }
+    ];
+
+    const mockTotais: Required<Alocacao> = { carteira: 'Total', financeiro: 300, planejado: 150, idCarteira: '1', classe: 'A (USD)', percentual: 0.5};
+
+    const mockAlocacoesComPercentual: Required<Alocacao>[] = [
+      { idCarteira: '1', classe: 'A (USD)', carteira: 'Carteira 1', financeiro: 100, planejado: 50, percentual: 0.3333 },
+      { idCarteira: '2', classe: 'B (EUR)', carteira: 'Carteira 2', financeiro: 200, planejado: 100, percentual: 0.6667 }
+    ];
+
+    const mockResultado: Alocacoes = { alocacoes: mockAlocacoesComPercentual, totais: mockTotais };
+
+    (investimentoStateService.calcularTotaisTodasCarteiras as jest.Mock).mockReturnValue(of(mockConsolidados));
+
+    service.consolidarAlocacoes().subscribe(result => {
+      expect(result).toEqual(mockResultado);
+      done();
+    });
   });
 });
--- app-patrimonio/src/app/services/consolidacao.service.ts
+++ app-patrimonio/src/app/services/consolidacao.service.ts
@@ -13,12 +13,21 @@ export class ConsolidacaoService {
 
   constructor() { }
 
-  consolidarAlocacoes () : Observable<Alocacoes>{
+  /**
+  * Consolida os dados de alocação de todos os cartões de investimentos.
+  *
+  * Esta função busca os dados totais de alocação de cada carteira usando o `InvestimentoStateService`.
+  * Em seguida, calcula os valores totais de finanças e planejados, e mapeia os dados de alocação para um novo formato.
+  * Por fim, calcula a porcentagem de cada alocação em relação ao valor total de finanças.
+  *
+  * @returns Um Observable de `Alocacoes` contendo os dados de alocação consolidados.
+  */
+  consolidarAlocacoes(): Observable<Alocacoes> {
     return this.investimentoStateService.calcularTotaisTodasCarteiras().pipe(
-      map(consolidados=>{
-        let alocacoes : Alocacao[] = consolidados.map(consolidado=>{
-          const alocacao : Alocacao = {
-            id: consolidado.identity.toString(),
+      map(consolidados => {
+        let alocacoes: Alocacao[] = consolidados.map(consolidado => {
+          const alocacao: Alocacao = {
+            idCarteira: consolidado.identity.toString(),
             classe: `${consolidado.classe} (${consolidado.moeda})`,
             carteira: consolidado.nome,
             financeiro: this.investimentoStateService.converteParaMoeda(consolidado.moeda, Moeda.BRL, consolidado.vlTotal),
@@ -26,15 +35,15 @@ export class ConsolidacaoService {
           };
           return alocacao;
         });
-  
+
         const totais = this.totais(alocacoes);
-  
-        return {alocacoes, totais};
+
+        return { alocacoes, totais };
       }),
-      map(calculado=>{
-        const totalPlanejado = calculado.alocacoes.reduce((acc,v)=>acc+=this.percentualFinanceiro(v, calculado.totais),0)
-        const alocacoes = calculado.alocacoes.map(alocacao=>({...alocacao, percentual: this.percentualFinanceiro(alocacao, calculado.totais)}));
-        return {alocacoes, totais: {...calculado.totais, percentual: totalPlanejado}};
+      map(calculado => {
+        const totalPlanejado = calculado.alocacoes.reduce((acc, v) => acc += this.percentualFinanceiro(v, calculado.totais), 0)
+        const alocacoes = calculado.alocacoes.map(alocacao => ({ ...alocacao, percentual: this.percentualFinanceiro(alocacao, calculado.totais) }));
+        return { alocacoes, totais: { ...calculado.totais, percentual: totalPlanejado } };
       }))
   }
 
@@ -44,7 +53,7 @@ export class ConsolidacaoService {
       let cons = acc.get(alocacao.classe) as Required<Alocacao>;
       if (!cons) {
         cons = {
-          id: tipo,
+          idCarteira: tipo,
           classe: alocacao.classe,
           carteira: tipo,
           financeiro: 0,
@@ -62,19 +71,19 @@ export class ConsolidacaoService {
     consolidado.alocacoes = Array.from(mapPorClasse.values())
     return consolidado;
   }
-  
-  private totais (alocacoes: Alocacao[]) {
+
+  private totais(alocacoes: Alocacao[]) {
     return alocacoes.reduce(
       (acc, curr) => ({
         ...acc,
         financeiro: acc.financeiro + curr.financeiro,
         planejado: acc.planejado + curr.planejado
       }),
-      {carteira: 'Total', financeiro: 0, planejado: 0} as Alocacao);
+      { carteira: 'Total', financeiro: 0, planejado: 0 } as Alocacao);
   }
 
   percentualFinanceiro(alocacao: Alocacao, totais: Alocacao): number {
-    if(totais.financeiro === 0) return 0;
+    if (totais.financeiro === 0) return 0;
     return alocacao.financeiro / totais.financeiro;
   }
 
